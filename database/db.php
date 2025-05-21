3ddd6e450cbd662c8d40c0c529ceb3389777895e
santino <ing.roquezegarra@gmail.com>
2025-05-20 23:02:49 -0500

    add port

--- database/db.php
+++ database/db.php
@@ -3,10 +3,11 @@
 function getConnection(): PDO
 {
     $host = getenv('DB_HOST');
+    $port = getenv('DB_PORT'); // ← ¡Aquí el puerto!
     $db   = getenv('DB_NAME');
     $user = getenv('DB_USER');
     $pass = getenv('DB_PASS');
-    $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
+    $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 
     try {
         return new PDO($dsn, $user, $pass, [
