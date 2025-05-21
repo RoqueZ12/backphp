16edbf4251b8559eea5726cb59ce0ec612e0a3f7
santino <ing.roquezegarra@gmail.com>
2025-05-21 09:33:46 -0500

    feat api

--- database/db.php
+++ database/db.php
@@ -3,7 +3,7 @@
 function getConnection(): PDO
 {
     $host = getenv('DB_HOST');
-    $port = getenv('DB_PORT'); // ← ¡Aquí el puerto!
+    $port = getenv('DB_PORT');
     $db   = getenv('DB_NAME');
     $user = getenv('DB_USER');
     $pass = getenv('DB_PASS');
--- public/productos/index.php
+++ public/productos/index.php
@@ -2,33 +2,12 @@
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../database/db.php';
 
-use App\FirebaseAuth;
-
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: https://reacfront.vercel.app");
 header("Access-Control-Allow-Headers: Content-Type");
 
 $method = $_SERVER['REQUEST_METHOD'];
 $pdo = getConnection();
-
-// Validación Firebase opcional:
-$authHeader = $_SERVER['HTTP_AUTHORIZATION'] ?? '';
-$token = str_replace('Bearer ', '', $authHeader);
-
-$credentials = getenv('FIREBASE_CREDENTIALS');
-if (!$credentials || !$token) {
-    http_response_code(401);
-    echo json_encode(['error' => 'No autorizado']);
-    exit;
-}
-try {
-    $auth = new FirebaseAuth($credentials);
-    $auth->verifyToken($token); // Si es válido, continúa
-} catch (Exception $e) {
-    http_response_code(403);
-    echo json_encode(['error' => 'Token inválido']);
-    exit;
-}
 
 // Manejo de rutas
 switch ($method) {
@@ -45,15 +24,15 @@ switch ($method) {
 
     case 'POST':
         $input = json_decode(file_get_contents("php://input"), true);
-        $stmt = $pdo->prepare("INSERT INTO productos (nombre, precio, imagen) VALUES (?, ?, ?)");
-        $stmt->execute([$input['nombre'], $input['precio'], $input['imagen']]);
+        $stmt = $pdo->prepare("INSERT INTO productos (nombre, cantidad, precio, image) VALUES (?, ?, ?, ?)");
+        $stmt->execute([$input['nombre'], $input['precio'], $input['imagen'], $input['cantidad']]);
         echo json_encode(['success' => true, 'id' => $pdo->lastInsertId()]);
         break;
 
     case 'PUT':
         $input = json_decode(file_get_contents("php://input"), true);
-        $stmt = $pdo->prepare("UPDATE productos SET nombre=?, precio=?, imagen=? WHERE id=?");
-        $stmt->execute([$input['nombre'], $input['precio'], $input['imagen'], $input['id']]);
+        $stmt = $pdo->prepare("UPDATE productos SET nombre=?, precio=?, imagen=? , cantidad=? WHERE id=?");
+        $stmt->execute([$input['nombre'], $input['precio'], $input['imagen'], $input['id'], $input['cantidad']]);
         echo json_encode(['success' => true]);
         break;
 
